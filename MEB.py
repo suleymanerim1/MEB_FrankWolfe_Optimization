c74ec091412bf574849aa55b2eb5c3d9b95ac1f5
suleymanerim1 <erim_suleyman@hotmail.com>
2023-08-18 21:55:32 +0200

    debugged, now both algorithms should work w/out problem. We should check with graphs.

--- MEB.py
+++ MEB.py
@@ -1,6 +1,6 @@
 import numpy as np
 from src.logger import logging
-from src.Frank_Wolfe import FW_Q
+from src.Frank_Wolfe import awayStep_FW, blendedPairwise_FW
 
 if __name__ == '__main__':
 
@@ -9,28 +9,57 @@ if __name__ == '__main__':
     # Number of samples
     m = 2 ** 5
 
-    # Number of variables
+    # Dimension of variables
     n = 2 ** 4
 
     # Max number of iteartions
-    maxit = 3000
+    maxit = 50000
 
     # initial solution
-    u0 = np.zeros(n)
+    u0 = np.zeros(m)
     u0[0] = 1e0
 
-    A = np.random.randn(m, n)
+    A = np.random.randn(n, m)
 
     eps = 1e-6
 
     print("*****************")
-    print("*  FW STANDARD  *")
+    print("*  Away Step FW   *")
     print("*****************")
 
-    logging.info("Frank Wolfe algorithm started!")
-    u_fw, iter_fw, dual_fw = FW_Q(A, u0, eps, maxit)
+    logging.info("\nASFW algorithm started!")
+    u_fw, iter_fw, dual_fw, CPU_time_fw = awayStep_FW(A, u0, eps, maxit)
+    radius_awayStep_FW = np.sqrt(-dual_fw)
+    a_sum = np.sum(A,axis=0)
+    center_awayStep_FW = a_sum * u_fw
 
     # Print results:
     print(f"dual function = {dual_fw:.3e}")
     print(f"Number of non-zero components of x = {np.sum(np.abs(u_fw) >= 0.0001)}")
     print(f"Number of iterations = {iter_fw}")
+    print(f"CPU time: {CPU_time_fw}")
+    print(f"center: {center_awayStep_FW} and radius: {radius_awayStep_FW} ")
+
+    logging.info("Away step Frank Wolfe finished!")
+    logging.info(f"center: {center_awayStep_FW} and radius: {radius_awayStep_FW} ")
+
+
+    print("*****************")
+    print("*  Blended Pairwise FW   *")
+    print("*****************")
+
+    logging.info("\nBPFW algorithm started!")
+    u_bp, iter_bp, dual_bp, CPU_time_bp = blendedPairwise_FW(A, u0, eps, maxit)
+    radius_awayStep_BP = np.sqrt(-dual_bp)
+    a_sum = np.sum(A,axis=0)
+    center_awayStep_BP = a_sum * u_bp
+
+    # Print results:
+    print(f"dual function = {dual_bp:.3e}")
+    print(f"Number of non-zero components of x = {np.sum(np.abs(dual_bp) >= 0.0001)}")
+    print(f"Number of iterations = {iter_bp}")
+    print(f"CPU time: {CPU_time_bp}")
+    print(f"center: {center_awayStep_BP} and radius: {radius_awayStep_BP} ")
+
+    logging.info("BP Frank Wolfe finished!")
+    logging.info(f"center: {center_awayStep_BP} and radius: {radius_awayStep_BP} ")
--- src/Frank_Wolfe.py
+++ src/Frank_Wolfe.py
@@ -3,15 +3,10 @@ import math
 import time
 from src.logger import logging
 
-# TODO: away step FW rewrite step by step, change variables names like blended Pairwise, use dual_func and gradient and line search
-# TODO: away step arrange active set weights by article
-# TODO: blended pairwise think about active set weights
-# TODO: return radius and center
 # TODO: blended does not have stopping condition, why?
 # TODO: add last algorithm
 # TODO: add stopping condition of time for each alogirthm
 # TODO: add max iteration stopping condition for each algorithm if neccesaary
-# TODO: min grad stopping condition for each algorithm if neceesary
 # TODO: run each algorithm and check for error from logs and results, check how each variables acts according to article
 def dual_function(A, u):
     A2 = A.T @ A
@@ -27,12 +22,15 @@ def gradient(A, u):
 
     return 2 * A2u - sum_a2
 
-def golden_section_search(func, a , b, tol=1e-6, max_iter=100):
+def golden_section_search(func,A,u,d_t,a,b, tol=1e-6, max_iter=100):
     """
     Perform exact line search using the golden section search method.
 
     Parameters:
         func (callable): The objective function to minimize.
+        A (matrix): points matrix (m,n)
+        u (vector) : convex combination weights for MEB dual problem
+        d (vector) : search direction for line search
         a (float): The left endpoint of the interval.
         b (float): The right endpoint of the interval.
         tol (float): Tolerance for stopping criterion.
@@ -47,25 +45,29 @@ def golden_section_search(func, a , b, tol=1e-6, max_iter=100):
     x1 = a + (1 - golden_ratio) * (b - a)
     x2 = a + golden_ratio * (b - a)
 
-    f_x1 = func(x1)
-    f_x2 = func(x2)
+    u1 = u+d_t*x1
+    u2 = u+d_t*x2
+    dual1 = func(A,u1)
+    dual2 = func(A,u2)
 
     for _ in range(max_iter):
         if abs(b - a) < tol:
             break
 
-        if f_x1 < f_x2:
+        if dual1 < dual2:
             b = x2
             x2 = x1
-            f_x2 = f_x1
+            dual2 = dual1
             x1 = a + (1 - golden_ratio) * (b - a)
-            f_x1 = func(x1)
+            u1 = u + d_t * x1
+            dual1 = func(A,u1)
         else:
             a = x1
             x1 = x2
-            f_x1 = f_x2
+            dual1 = dual2
             x2 = a + golden_ratio * (b - a)
-            f_x2 = func(x2)
+            u2 = u + d_t * x2
+            dual2 = func(A,u2)
 
     return (a + b) / 2
 
@@ -74,20 +76,20 @@ def golden_section_search(func, a , b, tol=1e-6, max_iter=100):
 
 def awayStep_FW(A, u, eps, maxit):
     #Lacoste-Julien
+    logging.info("Away Step Frank Wolfe algorithm first iteration started!")
     tstart = time.time()
 
 
-    _, n = A.shape
+    _, m = A.shape
 
 
     alpha_max = 1  # max step_size
     alpha_t = 0  # step_size at iteration t
 
     # step 1
-    St = np.where(u > 0)[0]  # active set St -- initialize
-
-
-    logging.info("Away Step Frank Wolfe algorithm first iteration started!")
+    logging.info("Active set initialized!")
+    St = np.zeros(m)
+    St[np.where(u > 0)[0]] = 1  # active set St -- initialize
 
     # step 2
     for t in range(maxit):
@@ -105,7 +107,7 @@ def awayStep_FW(A, u, eps, maxit):
 
         # solution of FW problem - step 3
         s_t_idx = np.argmin(grad) #find index which makes gradient min
-        s_t = np.zeros(n)  # create n dimensional array and do e_i = 1
+        s_t = np.zeros(m)  # create n dimensional array and do e_i = 1
         s_t[s_t_idx] = 1.0
         logging.info(f"FW min grad unit simplex index found: {s_t_idx}")
 
@@ -115,9 +117,10 @@ def awayStep_FW(A, u, eps, maxit):
 
 
         # solution of Away Step - step 4 in Lacoste-Julien algo1
-        max_grad_idx = np.argmax(grad[St])  # find max_grad_index from active set St
-        v_t_idx = St[max_grad_idx]  # set away vertex index from active set St
-        v_t = np.zeros(n)
+        active_idxs = np.where(St > 0)[0]
+        max_grad_idx = np.argmax(grad[active_idxs])  # find max_grad_index from active set St
+        v_t_idx = active_idxs[max_grad_idx]  # set away vertex index from active set St
+        v_t = np.zeros(m)
         v_t[v_t_idx] = 1.0  # create n dimensional array and do a_t = 1 and others 0
         logging.info(f"Away step max grad unit simplex index found: {v_t_idx}")
 
@@ -131,7 +134,7 @@ def awayStep_FW(A, u, eps, maxit):
         g_FW = -grad.T @ d_FW
         if g_FW  <= eps:
             logging.info(f"Stopping condition gap < epsilon is met!")
-            return u
+            break
 
         # step 6 - compare FW gap and AW gap
         g_AW = -grad.T @ d_AW  # calculate Away step gap
@@ -139,7 +142,7 @@ def awayStep_FW(A, u, eps, maxit):
         if g_FW >= g_AW:
 
             #step 7 - set direction at iteration k as FW direction
-            d_k = d_FW
+            d_t = d_FW
             alpha_max = 1  # set max step_size
             logging.info(f"Choose Frank Wolfe direction, step_size_max: {alpha_max}")
 
@@ -148,24 +151,24 @@ def awayStep_FW(A, u, eps, maxit):
         else:
 
             #step 9 - choose away direction and max feasible step_size
-            d_k = d_AW
+            d_t = d_AW
             alpha_max = St[v_t_idx] / (1-St[v_t_idx]) # use the value of unit simplex highest grad coordinate
             logging.info(f"Choose Away Step  direction, step_size_max : {alpha_max}")
             fw_check = False
 
         #step 10 end if
         # step 11- a line search
-        alpha_t = golden_section_search(dual(A, u), a=0, b=alpha_max)
+        alpha_t = golden_section_search(dual_function,A,u,d_t, a=0, b=alpha_max)
 
-        logging.info(f"Step size is set {alpha_t}")
+        logging.info(f"Step size is set--> alpha_t: {alpha_t}")
 
         #step 12
-        u = u + alpha_t * d_k
+        u = u + alpha_t * d_t
         logging.info(f"u values updated")
 
         # step 13 update active set (Lacoste-Juline pg.4 1st paragraph)
         if fw_check:  # fw step used
-                St= (1-alpha_t)*St
+                St = (1-alpha_t)*St
                 St[s_t_idx] = St[s_t_idx]+alpha_t
 
 
@@ -173,10 +176,8 @@ def awayStep_FW(A, u, eps, maxit):
             St = (1+alpha_t) * St
             St[v_t_idx] = St[v_t_idx] - alpha_t
 
-        logging.info(f"Active set is updated!:{St}")
+        logging.info(f"Active set is updated!")
         # step 14 end for
-
-
 
     ttot = time.time() - tstart
 
@@ -184,24 +185,28 @@ def awayStep_FW(A, u, eps, maxit):
     logging.info(f"Last value of dual function {dual:.3e}")
     logging.info(f"Total CPU time {ttot:.3e}")
 
-    return u, t, dual
+    return (u,t, dual, ttot)
 
 
 def blendedPairwise_FW(A, u, eps, maxit):
+    logging.info("Blended Pairwise Frank Wolfe algorithm first iteration started!")
 
     # Tsuji Algorithm 1
     tstart = time.time()
 
-    _, n = A.shape
+    _, m = A.shape
 
     alpha_max = 1  # max step_size
     alpha_t = 0  # step_size at iteration t
 
 
     # step 1
-    St = np.where(u > 0)[0]  # active set St -- initialize
 
-    logging.info("Blended Pairwise Frank Wolfe algorithm first iteration started!")
+    St = np.zeros(m)
+    St[np.where(u > 0)[0]] = 1  # active set St -- initialize
+    logging.info("Active set initialized!")
+
+
     # step 2
     for t in range(maxit):
         logging.info(f"\n--------------Iteration {t} -----------------")
@@ -216,22 +221,24 @@ def blendedPairwise_FW(A, u, eps, maxit):
         logging.info(f"Gradient calculation done. ")
 
         # step 3 - away vertex
-        max_grad_idx = np.argmax(grad[St])  # find max_grad_index from active set St
-        a_t_idx = St[max_grad_idx]  # set away vertex index from active set St
-        a_t = np.zeros(n)
+        active_idxs = np.where(St > 0)[0]
+        max_grad_idx = np.argmax(grad[active_idxs])  # find max_grad_index from active set St
+        a_t_idx = active_idxs[max_grad_idx]  # set away vertex index from active set St
+        a_t = np.zeros(m)
         a_t[a_t_idx] = 1.0  # create n dimensional array and do a_t = 1 and others 0
         logging.info(f"Away step max grad unit simplex index found: {a_t_idx}")
 
+
         # step 4 - local FW
-        min_grad_idx = np.argmin(grad[St])  # find min_grad_index from active set St
-        s_t_idx = St[min_grad_idx]  # set local FW index from active set St
-        s_t = np.zeros(n)
+        min_grad_idx = np.argmin(grad[active_idxs])  # find min_grad_index from active set St
+        s_t_idx = active_idxs[min_grad_idx]  # set local FW index from active set St
+        s_t = np.zeros(m)
         s_t[s_t_idx] = 1.0  # create n dimensional array and do s_t = 1 and others 0
         logging.info(f"Away step max grad unit simplex index found: {s_t_idx}")
 
         # step 5 - global FW
         w_t_idx = np.argmin(grad)  # find index which makes gradient min - search in all vertices
-        w_t = np.zeros(n)
+        w_t = np.zeros(m)
         w_t[w_t_idx] = 1.0  # create n dimensional array and do w_t = 1 and others 0
         logging.info(f"FW min grad unit simplex index found: {w_t_idx}")
 
@@ -245,16 +252,19 @@ def blendedPairwise_FW(A, u, eps, maxit):
             alpha_max = u[a_t_idx]
 
             # step 9
-            alpha_t = alpha_t  = golden_section_search(dual(A,u), a =0 , b = alpha_max)
+            alpha_t  = golden_section_search(dual_function,A,u,d_t, a =0 , b = alpha_max)
             # step 10
             if alpha_t < alpha_max:
                 # step 11
                 St = St  # descent step, do not update St
+                logging.info(f"Descent step taken, step_size: {alpha_t}")
 
             # step 12
             else:
                 # step 13
                 St[a_t_idx] = 0  # drop step
+                logging.info(f"Drop step taken, step_size: {alpha_t}")
+
         # step 14 - end if
         # step 15
         else:  # FW Step
@@ -262,16 +272,20 @@ def blendedPairwise_FW(A, u, eps, maxit):
             d_t = u - w_t  # u weights - global FW
 
             # step 17
-            alpha_t  = golden_section_search(dual(A,u), a =0 , b = 1)
+            alpha_t  = golden_section_search(dual_function,A,u,d_t, a =0 , b = 1)
+            logging.info(f"Choose Frank Wolfe step taken, step_size: {alpha_t}")
             # step 18
             if alpha_t == 1:
                 St = w_t
             else:
                 St[w_t_idx] = 1
+            logging.info(f"Active set is updated!")
 
         # step 19 - end if
         # step 20
         u = u - alpha_t * d_t
+        logging.info(f"u values updated")
+
     # step 21 - end for
 
     ttot = time.time() - tstart
@@ -280,6 +294,6 @@ def blendedPairwise_FW(A, u, eps, maxit):
     logging.info(f"Last value of dual function {dual:.3e}")
     logging.info(f"Total CPU time {ttot:.3e}")
 
-    return u, t, dual
+    return (u, t, dual, ttot)
 
 
